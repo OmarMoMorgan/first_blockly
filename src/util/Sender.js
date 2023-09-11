9a0c254c421951fe70d2be1b8099729ac2804ef8
OmarMoMorgan <s-omar.morgan@zewailcity.edu.eg>
2023-09-11 06:01:56 +0300

    added check to writeSerialPort

--- src/util/Sender.js
+++ src/util/Sender.js
@@ -1,21 +1,35 @@
 const { SerialPort } = require('serialport')
+const { ReadlineParser } = require('@serialport/parser-readline')
+
+let isReady = 0;
 
 function makeSerialPort(path_port) {
     port = new SerialPort({
-    path:path_port, //this hard coded and will be changed in the future of course
+    path:path_port, 
     baudRate: 9600,
     });
     console.log("port made")
+    //making node listen now to what the board sends
+    const parser = port.pipe(new ReadlineParser({ delimiter: '\r\n' }))
+      parser.on('data', (data)=>{
+        if (data.trim() === "ok"){
+            isReady = 1;
+            console.log("heyyy i got an okayyy")
+        }
+        console.log("data received is",data);
+    })
     return port;
 }
 
 function writeSerialPortSimple(port,data){
-    if (port) {
+    data = data + "\n";
+    if (port && isReady === 1) {
         port.write(data, (err) => {
           if (err) {
             console.error('Error writing to port:', err);
           } else {
             console.log('Data written to arduino:', data);
+            isReady = 0;
           }
         });
     }
